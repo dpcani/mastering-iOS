0c3e50d1c331a61b50f75e3ee468171a1c5130d1
David de la Puente <david.puente@marbleinteractive.com>
2023-01-07 08:30:10 +0100

    Refactor code, used Group to specify properties for all elements

--- SwiftUIScrollView/SwiftUIScrollView/ContentView.swift
+++ SwiftUIScrollView/SwiftUIScrollView/ContentView.swift
@@ -24,14 +24,13 @@ struct ContentView: View {
                 Spacer()
             }
             HStack {
-                CardView(image: "swiftui-button", category: "SwiftUI", heading: "Drawing a Border with Rounded Corners", author: "Simon Ng")
-                    .frame(width: 300)
-                CardView(image: "macos-programming", category: "macOS", heading: "Building a Simple Editing App", author: "Gabriel Theodoropoulos")
-                    .frame(width: 300)
-                CardView(image: "flutter-app", category: "Flutter", heading: "Building a Complex Layout with Flutter", author: "Lawrence Tan")
-                    .frame(width: 300)
-                CardView(image: "natural-language-api", category: "iOS", heading: "What's New in Natural Language API", author: "Sai Kambampati")
-                    .frame(width: 300)
+                Group{
+                    CardView(image: "swiftui-button", category: "SwiftUI", heading: "Drawing a Border with Rounded Corners", author: "Simon Ng")
+                    CardView(image: "macos-programming", category: "macOS", heading: "Building a Simple Editing App", author: "Gabriel Theodoropoulos")
+                    CardView(image: "flutter-app", category: "Flutter", heading: "Building a Complex Layout with Flutter", author: "Lawrence Tan")
+                    CardView(image: "natural-language-api", category: "iOS", heading: "What's New in Natural Language API", author: "Sai Kambampati")
+                }
+                .frame(width: 300)
             }
         }
     }
