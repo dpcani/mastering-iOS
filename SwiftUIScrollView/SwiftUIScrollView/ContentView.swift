8d8ef3459e20e0a0072f67c0e0a791fa07433b3e
David de la Puente <david.puente@marbleinteractive.com>
2023-01-07 08:34:58 +0100

    Refactor code to fix the title when the user scrolls the cards Add Spacer to put the content at the top

--- SwiftUIScrollView/SwiftUIScrollView/ContentView.swift
+++ SwiftUIScrollView/SwiftUIScrollView/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ScrollView(.horizontal, showsIndicators: false) {
+        VStack{
             HStack {
                 VStack (alignment: .leading){
                     Text("MONDAY, JAN 7")
@@ -23,15 +23,18 @@ struct ContentView: View {
                 .padding(.horizontal)
                 Spacer()
             }
-            HStack {
-                Group{
-                    CardView(image: "swiftui-button", category: "SwiftUI", heading: "Drawing a Border with Rounded Corners", author: "Simon Ng")
-                    CardView(image: "macos-programming", category: "macOS", heading: "Building a Simple Editing App", author: "Gabriel Theodoropoulos")
-                    CardView(image: "flutter-app", category: "Flutter", heading: "Building a Complex Layout with Flutter", author: "Lawrence Tan")
-                    CardView(image: "natural-language-api", category: "iOS", heading: "What's New in Natural Language API", author: "Sai Kambampati")
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack {
+                    Group{
+                        CardView(image: "swiftui-button", category: "SwiftUI", heading: "Drawing a Border with Rounded Corners", author: "Simon Ng")
+                        CardView(image: "macos-programming", category: "macOS", heading: "Building a Simple Editing App", author: "Gabriel Theodoropoulos")
+                        CardView(image: "flutter-app", category: "Flutter", heading: "Building a Complex Layout with Flutter", author: "Lawrence Tan")
+                        CardView(image: "natural-language-api", category: "iOS", heading: "What's New in Natural Language API", author: "Sai Kambampati")
+                    }
+                    .frame(width: 300)
                 }
-                .frame(width: 300)
             }
+            Spacer()
         }
     }
 }
