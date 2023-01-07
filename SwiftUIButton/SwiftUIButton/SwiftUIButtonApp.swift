3ff00fb95169c276135027f2f53d73a834b4dbee
David de la Puente <david.puente@marbleinteractive.com>
2023-01-07 09:34:26 +0100

    SwiftUI Button init

---
+++ SwiftUIButton/SwiftUIButton/SwiftUIButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIButtonApp.swift
+//  SwiftUIButton
+//
+//  Created by Marble Interactive SL on 7/1/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
