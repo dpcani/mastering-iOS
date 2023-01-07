1612297334047a7895dc1c2a1c80647443e9f2d0
David de la Puente <david.puente@marbleinteractive.com>
2023-01-07 09:04:52 +0100

    Add cover

--- SwiftUIScrollView/SwiftUIScrollView/ContentView.swift
+++ SwiftUIScrollView/SwiftUIScrollView/ContentView.swift
@@ -23,15 +23,15 @@ struct ContentView: View {
                 .padding(.horizontal)
                 Spacer()
             }
-            ScrollView(.horizontal, showsIndicators: false) {
-                HStack {
+            ScrollView {
+                VStack {
                     Group{
                         CardView(image: "swiftui-button", category: "SwiftUI", heading: "Drawing a Border with Rounded Corners", author: "Simon Ng")
                         CardView(image: "macos-programming", category: "macOS", heading: "Building a Simple Editing App", author: "Gabriel Theodoropoulos")
                         CardView(image: "flutter-app", category: "Flutter", heading: "Building a Complex Layout with Flutter", author: "Lawrence Tan")
                         CardView(image: "natural-language-api", category: "iOS", heading: "What's New in Natural Language API", author: "Sai Kambampati")
                     }
-                    .frame(width: 300)
+                   
                 }
             }
             Spacer()
