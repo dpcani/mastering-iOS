1dc77ab5f48a6644256b405a9bec54747e1a2b4a
David de la Puente <david.puente@marbleinteractive.com>
2023-01-07 10:18:14 +0100

    Gradient Button with shadow

--- SwiftUIButton/SwiftUIButton/ContentView.swift
+++ SwiftUIButton/SwiftUIButton/ContentView.swift
@@ -45,8 +45,9 @@ struct ContentView: View {
             )
             .padding()
             .foregroundColor(.white)
-            .background(LinearGradient(gradient: Gradient(colors: [Color.red, Color.blue]), startPoint: .leading, endPoint: .trailing))
+            .background(LinearGradient(gradient: Gradient(colors: [Color("DarkGreen"), Color("LightGreen")]), startPoint: .leading, endPoint: .trailing))
             .cornerRadius(40)
+            .shadow(color: .gray.opacity(0.7), radius: 10.0, x: 10, y: 10)
         }
     }
 }
