381ea7f6a99d2c1deeaa38244c3abe339aced32b
David de la Puente <david.puente@marbleinteractive.com>
2023-01-07 08:11:05 +0100

    Create CardView

---
+++ SwiftUIScrollView/SwiftUIScrollView/CardView.swift
@@ -0,0 +1,48 @@
+//
+//  CardView.swift
+//  SwiftUIScrollView
+//
+//  Created by Marble Interactive SL on 7/1/23.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        VStack {
+            Image("swiftui-button")
+                 .resizable()
+                 .aspectRatio(contentMode: .fit)
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("SwiftUI")
+                        .font(.headline)
+                        .foregroundColor(.secondary)
+                    Text("Drawing a Border with Rounded Corners")
+                        .font(.title)
+                        .fontWeight(.black)
+                        .foregroundColor(.primary)
+                        .lineLimit(3)
+                    Text("Written by Simon Ng".uppercased())
+                        .font(.caption)
+                        .foregroundColor(.secondary)
+                }
+                Spacer()
+            }
+            .padding()
+        }
+        .cornerRadius(10)
+        .overlay(
+            RoundedRectangle(
+                cornerRadius: 10)
+                .stroke(Color(.sRGB, red: 150/255, green: 150/255, blue: 150/255, opacity: 0.1), lineWidth: 1)
+        )
+        .padding([.top, .horizontal])
+    }
+}
+
+struct CardView_Previews: PreviewProvider {
+    static var previews: some View {
+        CardView()
+    }
+}
---
+++ SwiftUIScrollView/SwiftUIScrollView/SwiftUIScrollViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIScrollViewApp.swift
+//  SwiftUIScrollView
+//
+//  Created by Marble Interactive SL on 7/1/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIScrollViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
