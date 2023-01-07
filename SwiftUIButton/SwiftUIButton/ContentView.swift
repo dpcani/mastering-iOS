d6d5e877ac679a30fcc9d06cabfd28a3985a3b47
David de la Puente <david.puente@marbleinteractive.com>
2023-01-07 09:39:11 +0100

    Create a simple button

--- SwiftUIButton/SwiftUIButton/ContentView.swift
+++ SwiftUIButton/SwiftUIButton/ContentView.swift
@@ -10,10 +10,11 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Button {
+               print("Button pressed!")
+            } label: {
+                Text("Hello World")
+            }
         }
         .padding()
     }
