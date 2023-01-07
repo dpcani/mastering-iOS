f87c0cb9708262ce4a62e562c176e1b4c987ce3a
David de la Puente <david.puente@marbleinteractive.com>
2023-01-07 08:27:03 +0100

    Convert vertical scrollview in horizontal scrollview

--- SwiftUIScrollView/SwiftUIScrollView/ContentView.swift
+++ SwiftUIScrollView/SwiftUIScrollView/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ScrollView {
+        ScrollView(.horizontal) {
             HStack {
                 VStack (alignment: .leading){
                     Text("MONDAY, JAN 7")
@@ -23,11 +23,15 @@ struct ContentView: View {
                 .padding(.horizontal)
                 Spacer()
             }
-            VStack {
+            HStack {
                 CardView(image: "swiftui-button", category: "SwiftUI", heading: "Drawing a Border with Rounded Corners", author: "Simon Ng")
+                    .frame(width: 300)
                 CardView(image: "macos-programming", category: "macOS", heading: "Building a Simple Editing App", author: "Gabriel Theodoropoulos")
+                    .frame(width: 300)
                 CardView(image: "flutter-app", category: "Flutter", heading: "Building a Complex Layout with Flutter", author: "Lawrence Tan")
+                    .frame(width: 300)
                 CardView(image: "natural-language-api", category: "iOS", heading: "What's New in Natural Language API", author: "Sai Kambampati")
+                    .frame(width: 300)
             }
         }
     }
