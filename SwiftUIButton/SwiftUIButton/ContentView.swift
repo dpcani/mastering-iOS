7aa1dbe886cce0b1c45017e482eaa52b8b52eddf
David de la Puente <david.puente@marbleinteractive.com>
2023-01-07 10:23:12 +0100

    Full with button with gradient background

--- SwiftUIButton/SwiftUIButton/ContentView.swift
+++ SwiftUIButton/SwiftUIButton/ContentView.swift
@@ -9,45 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        /* VStack {
-            Button {
-               print("Delete Button pressed!")
-            } label: {
-                HStack {
-                    Image(systemName: "trash")
-                        .font(.title)
-                    Text("Delete")
-                        .fontWeight(.semibold)
-                        .font(.title)
-                }
-                .padding()
-                .foregroundColor(.white)
-                .background(.red)
-                .cornerRadius(40)
+        Button(action: {
+            print("Delete tapped!")
+        }) {
+            HStack {
+                Image(systemName: "trash")
+                    .font(.title)
+                Text("Delete")
+                    .fontWeight(.semibold)
+                    .font(.title)
             }
-        }
-        .padding()
-         */
-        
-        //Same Button with Label and Icon
-        Button {
-           print("Delete button tapped")
-        } label: {
-            Label(
-                title: {
-                    Text("Delete")
-                        .fontWeight(.semibold)
-                        .font(.title)
-                }, icon: {
-                    Image(systemName: "trash")
-                        .font(.title)
-                }
-            )
+            .frame(minWidth: 0, maxWidth: .infinity)
             .padding()
             .foregroundColor(.white)
             .background(LinearGradient(gradient: Gradient(colors: [Color("DarkGreen"), Color("LightGreen")]), startPoint: .leading, endPoint: .trailing))
             .cornerRadius(40)
-            .shadow(color: .gray.opacity(0.7), radius: 10.0, x: 10, y: 10)
+            .padding(.horizontal, 20)
         }
     }
 }
