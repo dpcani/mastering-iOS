ef3429de4665462a79c02401f4a161750bf9ccda
David de la Puente <david.puente@marbleinteractive.com>
2023-01-08 21:25:51 +0100

    Create Circle with heart and tap action

---
+++ SwiftUIAnimation/SwiftUIAnimation/ContentView.swift
@@ -0,0 +1,39 @@
+//
+//  ContentView.swift
+//  SwiftUIAnimation
+//
+//  Created by Marble Interactive SL on 8/1/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var circleColorChanged = false
+    @State private var heartColorChanged = false
+    @State private var heartSizeChanged = false
+    
+    var body: some View {
+        ZStack {
+            Circle()
+                .frame(width: 200, height: 200)
+                .foregroundColor(circleColorChanged ? Color(.systemGray5) : .red)
+            
+            Image(systemName: "heart.fill")
+                .foregroundColor(heartColorChanged ? .red : .white)
+                .font(.system(size: 100))
+                .scaleEffect(heartSizeChanged ? 1.0 : 0.5)
+        }
+        .onTapGesture {
+            circleColorChanged.toggle()
+            heartSizeChanged.toggle()
+            heartColorChanged.toggle()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIAnimation/SwiftUIAnimation/SwiftUIAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAnimationApp.swift
+//  SwiftUIAnimation
+//
+//  Created by Marble Interactive SL on 8/1/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
