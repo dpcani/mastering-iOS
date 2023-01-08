e3444ec2b17c7be99cb7ada740ee04204748dbc7
David de la Puente <david.puente@marbleinteractive.com>
2023-01-08 21:44:24 +0100

    Loading animation

--- SwiftUIAnimation/SwiftUIAnimation/ContentView.swift
+++ SwiftUIAnimation/SwiftUIAnimation/ContentView.swift
@@ -9,27 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var circleColorChanged = false
-    @State private var heartColorChanged = false
-    @State private var heartSizeChanged = false
+    @State private var isLoading = false
     
     var body: some View {
         ZStack {
             Circle()
-                .frame(width: 200, height: 200)
-                .foregroundColor(circleColorChanged ? Color(.systemGray5) : .red)
-            
-            Image(systemName: "heart.fill")
-                .foregroundColor(heartColorChanged ? .red : .white)
-                .font(.system(size: 100))
-                .scaleEffect(heartSizeChanged ? 1.0 : 0.5)
-        }
-        .onTapGesture {
-            withAnimation(.spring(response: 0.3, dampingFraction: 0.3, blendDuration: 0.3)) {
-                circleColorChanged.toggle()
-                heartSizeChanged.toggle()
-                heartColorChanged.toggle()
-            }
+                .trim(from: 0, to: 0.7)
+                .stroke(.green, lineWidth: 5)
+                .frame(width: 100, height: 100)
+                .rotationEffect(Angle(degrees: isLoading ? 360 : 0))
+                .animation(.default.repeatForever(autoreverses: false), value: isLoading)
+                .onAppear() {
+                    isLoading = true
+                }
         }
     }
 }
