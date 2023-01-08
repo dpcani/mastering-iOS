c1d830f254279e154044246706a150d72710e948
David de la Puente <david.puente@marbleinteractive.com>
2023-01-08 08:51:50 +0100

    Add counter button

--- SwiftUIState/SwiftUIState/ContentView.swift
+++ SwiftUIState/SwiftUIState/ContentView.swift
@@ -8,8 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    var body: some View {
+        VStack {
+            PlayStopButton()
+            
+            CounterButton()
+        }
+    }
+}
+
+struct CounterButton: View {
     
-   @State private var isPlaying = false
+    @State private var counter = 0
+    
+    var body: some View {
+        Button{
+            counter = counter + 1
+        } label: {
+            Text("\(counter)")
+        }
+        .frame(width: 140, height: 140)
+        .background(.red)
+        .clipShape(Circle())
+        .foregroundColor(.white)
+        .font(.system(size: 60, design: .rounded))
+        .fontWeight(.bold)
+    }
+}
+
+struct PlayStopButton: View {
+    
+    @State private var isPlaying = false
     
     var body: some View {
         Button {
