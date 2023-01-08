09230bc9dacf285a88911e41cb6a3efdaabfbc73
David de la Puente <david.puente@marbleinteractive.com>
2023-01-08 22:03:51 +0100

    Linear progress animation

--- SwiftUIAnimation/SwiftUIAnimation/ContentView.swift
+++ SwiftUIAnimation/SwiftUIAnimation/ContentView.swift
@@ -13,14 +13,19 @@ struct ContentView: View {
     
     var body: some View {
         ZStack {
-            Circle()
-                .stroke(Color(.systemGray5), lineWidth: 14)
-                .frame(width: 100, height: 100)
-            Circle()
-                .trim(from: 0, to: 0.2)
-                .stroke(.green, lineWidth: 7)
-                .frame(width: 100, height: 100)
-                .rotationEffect(Angle(degrees: isLoading ? 360 : 0))
+            Text("Loading")
+                .font(.system(.body, design: .rounded))
+                .bold()
+                .offset(x: 0, y: -25)
+            
+            RoundedRectangle(cornerRadius: 3)
+                .stroke(Color(.systemGray5), lineWidth: 3)
+                .frame(width: 250, height: 3)
+            
+            RoundedRectangle(cornerRadius: 3)
+                .stroke(.green, lineWidth: 3)
+                .frame(width: 30, height: 3)
+                .offset(x: isLoading ? -110 : 110, y:0)
                 .animation(.linear(duration: 1).repeatForever(autoreverses: false), value: isLoading)
                 .onAppear() {
                     isLoading = true
