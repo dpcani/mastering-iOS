780950e9ebefce18ad9bf30b104c75683296e06c
David de la Puente <david.puente@marbleinteractive.com>
2023-01-08 22:13:50 +0100

    Dots loading indicator

--- SwiftUIAnimation/SwiftUIAnimation/ContentView.swift
+++ SwiftUIAnimation/SwiftUIAnimation/ContentView.swift
@@ -9,30 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var progress: CGFloat = 0.0
+    @State private var isLoading = false
     
     var body: some View {
-        ZStack {
-            Text("\(Int(progress * 100))%")
-                .font(.system(.body, design: .rounded))
-                .bold()
-            
-           Circle()
-                .stroke(Color(.systemGray5), lineWidth: 10)
-                .frame(width: 150, height: 150)
-            
-            Circle()
-                .trim(from: 0, to: progress)
-                .stroke(.green, lineWidth: 10)
-                .frame(width: 150, height: 150)
-                .rotationEffect(Angle(degrees: -90))
-        }
-        .onAppear() {
-            Timer.scheduledTimer(withTimeInterval: 0.5, repeats: true) { timer in
-                self.progress += 0.05
-                if self.progress >= 1 {
-                    timer.invalidate()
-                }
+        HStack {
+            ForEach(0...4, id: \.self) { index in
+                Circle()
+                    .frame(width: 10, height: 10)
+                    .foregroundColor(.green)
+                    .scaleEffect(self.isLoading ? 0 : 1)
+                    .animation(.linear(duration: 0.6).repeatForever().delay(0.2 * Double(index)), value: isLoading)
+            }
+            .onAppear() {
+                self.isLoading = true
             }
         }
     }
