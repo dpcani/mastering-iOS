8e8e9174b310091779ac9579fa672124406f57fe
David de la Puente <david.puente@marbleinteractive.com>
2023-01-08 10:13:31 +0100

    Pie Chart with 4 colours

--- SwiftUIShape/SwiftUIShape/ContentView.swift
+++ SwiftUIShape/SwiftUIShape/ContentView.swift
@@ -9,11 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Path() { path in
-            path.move(to: CGPoint(x: 200, y: 200))
-            path.addArc(center: .init(x: 200, y: 200), radius: 150, startAngle: .degrees(0), endAngle: .degrees(90), clockwise: true)
+        ZStack {
+            Path() { path in
+                path.move(to: CGPoint(x: 187, y: 187))
+                path.addArc(center: .init(x: 187, y: 187), radius: 150, startAngle: .degrees(0), endAngle: .degrees(190), clockwise: true)
+            }
+            .fill(.yellow)
+            
+            Path() { path in
+                path.move(to: CGPoint(x: 187, y: 187))
+                path.addArc(center: .init(x: 187, y: 187), radius: 150, startAngle: .degrees(190), endAngle: .degrees(110), clockwise: true)
+            }
+            .fill(.teal)
+            
+            Path() { path in
+                path.move(to: CGPoint(x: 187, y: 187))
+                path.addArc(center: .init(x: 187, y: 187), radius: 150, startAngle: .degrees(110), endAngle: .degrees(90), clockwise: true)
+            }
+            .fill(.blue)
+            
+            Path() { path in
+                path.move(to: CGPoint(x: 187, y: 187))
+                path.addArc(center: .init(x: 187, y: 187), radius: 150, startAngle: .degrees(90), endAngle: .degrees(360), clockwise: true)
+            }
+            .fill(.purple)
+            
+            
         }
-        .fill(.green)
     }
 }
 
