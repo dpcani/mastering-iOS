4419b8e08b1f429251cbf7bf603e6f99dc23d3ff
David de la Puente <david.puente@marbleinteractive.com>
2023-01-08 10:10:54 +0100

    Draw Simple Pie Chart

--- SwiftUIShape/SwiftUIShape/ContentView.swift
+++ SwiftUIShape/SwiftUIShape/ContentView.swift
@@ -9,29 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            Path() { path in
-                path.move(to: CGPoint(x: 20, y: 60))
-                path.addLine(to: CGPoint(x: 40, y: 60))
-                path.addQuadCurve(to: CGPoint(x: 210, y: 60), control: CGPoint(x: 125, y: 0))
-                path.addLine(to: CGPoint(x: 230, y: 60))
-                path.addLine(to: CGPoint(x: 230, y: 100))
-                path.addLine(to: CGPoint(x: 20, y: 100))
-                path.closeSubpath()
-            }
-            .fill(.purple)
-            
-            Path() { path in
-                path.move(to: CGPoint(x: 20, y: 60))
-                path.addLine(to: CGPoint(x: 40, y: 60))
-                path.addQuadCurve(to: CGPoint(x: 210, y: 60), control: CGPoint(x: 125, y: 0))
-                path.addLine(to: CGPoint(x: 230, y: 60))
-                path.addLine(to: CGPoint(x: 230, y: 100))
-                path.addLine(to: CGPoint(x: 20, y: 100))
-                path.closeSubpath()
-            }
-            .stroke(.black, lineWidth: 10)
+        Path() { path in
+            path.move(to: CGPoint(x: 200, y: 200))
+            path.addArc(center: .init(x: 200, y: 200), radius: 150, startAngle: .degrees(0), endAngle: .degrees(90), clockwise: true)
         }
+        .fill(.green)
     }
 }
 
