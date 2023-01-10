ef86fa476dfb0a61b53252559ce8ce1a5dd086b5
David de la Puente <david.puente@marbleinteractive.com>
2023-01-10 22:29:06 +0100

    Simple List with images

--- SwiftUIList/SwiftUIList/ContentView.swift
+++ SwiftUIList/SwiftUIList/ContentView.swift
@@ -8,21 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
-    var restaurantNames = ["Cafe Deadend", "Homei", "Teakha", "Cafe Loisl", "Petit e Oyster", "For Kee Restaurant", "Po's Atelier", "Bourke Street Bakery", "Haigh's Chocolate", "Palomino Espresso", "Upstate", "Traif", "Graham Avenue Meats And Deli" , "Waffle & Wolf", "Five Leaves", "Cafe Lore", "Confessional", "Barrafina", "Donos tia", "Royal Oak", "CASK Pub and Kitchen"]
-    
-    var restaurantImages = ["cafedeadend", "homei", "teakha", "cafeloisl", "petite oyster", "forkeerestaurant", "posatelier", "bourkestreetbakery", "haighschocolate" , "palominoespresso", "upstate", "traif", "grahamavenuemeats", "wafflewolf", "five leaves", "cafelore", "confessional", "barrafina", "donostia", "royaloak", "caskpub kitchen"]
+    var restaurants = [ Restaurant(name: "Cafe Deadend", image: "cafedeadend"),
+                        Restaurant(name: "Homei", image: "homei"),
+                        Restaurant(name: "Teakha", image: "teakha"),
+                        Restaurant(name: "Cafe Loisl", image: "cafeloisl"),
+                        Restaurant(name: "Petite Oyster", image: "petiteoyster"),
+                        Restaurant(name: "For Kee Restaurant", image: "forkeerestaurant"),
+                        Restaurant(name: "Po's Atelier", image: "posatelier"),
+                        Restaurant(name: "Bourke Street Bakery", image: "bourkestreetbakery"),
+                        Restaurant(name: "Haigh's Chocolate", image: "haighschocolate"),
+                        Restaurant(name: "Palomino Espresso", image: "palominoespresso"),
+                        Restaurant(name: "Upstate", image: "upstate"),
+                        Restaurant(name: "Traif", image: "traif"),
+                        Restaurant(name: "Graham Avenue Meats And Deli", image: "grahamavenuemeats"),
+                        Restaurant(name: "Waffle & Wolf", image: "wafflewolf"),
+                        Restaurant(name: "Five Leaves", image: "fiveleaves"),
+                        Restaurant(name: "Cafe Lore", image: "cafelore"),
+                        Restaurant(name: "Confessional", image: "confessional"),
+                        Restaurant(name: "Barrafina", image: "barrafina"),
+                        Restaurant(name: "Donostia", image: "donostia"),
+                        Restaurant(name: "Royal Oak", image: "royaloak"),
+                        Restaurant(name: "CASK Pub and Kitchen", image: "caskpubkitchen")]
+ 
     
     var body: some View {
         VStack {
-            List(restaurantNames.indices, id: \.self) { index in
+            List(restaurants) { restaurant in
                 HStack {
-                    Image(self.restaurantImages[index])
+                    Image(restaurant.image)
                         .resizable()
                         .frame(width: 40, height: 40)
                         .cornerRadius(5)
-                    Text(self.restaurantNames[index])
+                    Text(restaurant.name)
                 }
             }
+            .listStyle(.plain)
         }
     }
 }
@@ -31,4 +51,10 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+struct Restaurant: Identifiable {
+    var id = UUID()
+    var name: String
+    var image: String
 }
