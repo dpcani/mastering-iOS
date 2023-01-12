03f82b800032747a3c45a6ae3ee236d9c357278a
David de la Puente <david.puente@marbleinteractive.com>
2023-01-12 07:52:16 +0100

    Create master detail sample

---
+++ Navigation-ui-and-navigation-bar-customization/SwiftUINavigationList/ArticleDetailView.swift
@@ -0,0 +1,49 @@
+//
+//  SwiftUIArticleDetailView.swift
+//  SwiftUINavigationList
+//
+//  Created by Marble Interactive SL on 12/1/23.
+//
+
+import SwiftUI
+
+struct ArticleDetailView: View {
+    let article: Article
+    
+    var body: some View {
+        ScrollView {
+            VStack(alignment: .leading) {
+                Image(article.image)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 300)
+                Group {
+                    Text(article.title)
+                        .font(.system(.title, design: .rounded))
+                        .bold()
+                        .padding(.horizontal)
+                    Text("By \(article.author)".uppercased())
+                        .font(.system(.body, design: .rounded))
+                        .bold()
+                        .foregroundColor(.gray)
+                        .padding(.horizontal)
+                }
+                .padding(.bottom, 0)
+                .padding(.horizontal)
+                Text(article.excerpt)
+                    .font(.system(.body, design: .rounded))
+                    .padding()
+                    .lineLimit(1000)
+                    .multilineTextAlignment(.leading)
+                Spacer()
+            }
+        }
+    }
+}
+
+struct ArticleDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        let article = articles[0]
+        ArticleDetailView(article: article)
+    }
+}
---
+++ Navigation-ui-and-navigation-bar-customization/SwiftUINavigationList/SwiftUIArticlesView.swift
@@ -0,0 +1,68 @@
+//
+//  SwiftUIArticlesView.swift
+//  SwiftUINavigationList
+//
+//  Created by Marble Interactive SL on 12/1/23.
+//
+
+import SwiftUI
+
+struct SwiftUIArticlesView: View {
+    var body: some View {
+        NavigationStack {
+            List(articles) { article in
+                    
+                NavigationLink(destination: ArticleDetailView(article: article), label: { 
+                    ArticleRow(article: article)
+                })
+                 
+                .listRowSeparator(.hidden)
+            }
+            .listStyle(.plain)
+            .navigationTitle("Your reading")
+        }
+    }
+}
+
+struct SwiftUIArticlesView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIArticlesView()
+    }
+}
+
+struct ArticleRow: View {
+    var article: Article
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 6) {
+            Image(article.image)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .cornerRadius(5)
+            
+            Text(article.title)
+                .font(.system(.title, design: .rounded))
+                .fontWeight(.black)
+                .lineLimit(3)
+                .padding(.bottom, 0)
+            
+            Text("By \(article.author)".uppercased())
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                .padding(.bottom, 0)
+            
+            HStack(spacing: 3) {
+                ForEach(1...(article.rating), id: \.self) { _ in
+                    Image(systemName: "star.fill")
+                        .font(.caption)
+                        .foregroundColor(.yellow)
+                }
+            }
+            
+            Text(article.excerpt)
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+            
+        }
+    }
+}
