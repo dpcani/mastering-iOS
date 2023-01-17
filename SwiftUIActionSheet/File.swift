a419c631a6b85d3709e029f9bee6968fad80aa6b
David de la Puente <david.puente@marbleinteractive.com>
2023-01-17 22:00:36 +0100

    start project

---
+++ SwiftUIActionSheet/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  SwiftUIActionSheet
+//
+//  Created by Marble Interactive SL on 17/1/23.
+//
+
+import Foundation
---
+++ SwiftUIActionSheet/SwiftUIActionSheet/SwiftUIActionSheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIActionSheetApp.swift
+//  SwiftUIActionSheet
+//
+//  Created by Marble Interactive SL on 17/1/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIActionSheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
