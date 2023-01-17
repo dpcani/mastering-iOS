e1880029fefa2a0a60da244228945c35736a7c22
David de la Puente <david.puente@marbleinteractive.com>
2023-01-17 21:51:45 +0100

    Crate signup form with mvvm

---
+++ SwiftUIRegisterForm/SwiftUIRegisterForm/ContentView.swift
@@ -0,0 +1,124 @@
+//
+//  ContentView.swift
+//  SwiftUIRegisterForm
+//
+//  Created by Marble Interactive SL on 17/1/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @ObservedObject private var userRegistrationViewModel = UserRegistrationViewModel()
+    
+    var body: some View {
+        VStack {
+            Text("Create an account")
+                .font(.system(.largeTitle, design: .rounded))
+                .bold()
+                .padding(.bottom, 30)
+            
+            FormField(fieldValue: $userRegistrationViewModel.username, fieldName: "Username")
+            
+            RequirementText(iconColor: userRegistrationViewModel.isUsernameLengthValid ? Color.secondary : Color(red: 251/255, green: 128/255, blue: 128/255), text: "A minimum of 4 characters", isStrikeThrough: userRegistrationViewModel.isUsernameLengthValid)
+                .padding()
+            
+            FormField(fieldValue: $userRegistrationViewModel.password, fieldName: "Password", isSecure: true)
+            
+            VStack {
+                RequirementText(iconName: "lock.open", iconColor: userRegistrationViewModel.isPasswordLengthValid ? Color.secondary : Color(red: 251/255, green: 128/255, blue: 128/255), text: "A minimum of 8 characters", isStrikeThrough: userRegistrationViewModel.isPasswordLengthValid)
+              
+                RequirementText(iconName: "lock.open", iconColor: userRegistrationViewModel.isPasswordCapitalLetter ? Color.secondary : Color(red: 251/255, green: 128/255, blue: 128/255), text: "One uppercase letter", isStrikeThrough: userRegistrationViewModel.isPasswordCapitalLetter)
+            }
+            .padding()
+            
+            FormField(fieldValue: $userRegistrationViewModel.passwordConfirm, fieldName: "Confirm Password", isSecure: true)
+            
+            RequirementText(iconColor: userRegistrationViewModel.isPasswordConfirmValid ? Color.secondary : Color(red: 251/255, green: 128/255, blue: 128/255), text: "Your confirm password should be the same as password", isStrikeThrough: userRegistrationViewModel.isPasswordConfirmValid)
+                .padding()
+                .padding(.bottom, 50)
+            
+            Button(action: {
+                // Proceed to the next screen
+            }) {
+            Text("Sign Up")
+                .font(.system(.body, design: .rounded))
+                .foregroundColor(.white)
+                .bold()
+                .padding()
+                .frame(minWidth: 0, maxWidth: .infinity)
+                .background(LinearGradient(gradient: Gradient(colors: [Color(red:251/255, green: 128/255, blue: 128/255), Color(red: 253/255, green: 193/255, blue: 104/255)]), startPoint: .leading, endPoint: .trailing))
+                .cornerRadius(10)
+                .padding(.horizontal)
+            }
+            
+            HStack {
+                Text("Already have an account?")
+                    .font(.system(.body, design: .rounded))
+                    .bold()
+                
+                Button(action: {
+                    //action here
+                }){
+                    Text("Sign In")
+                        .font(.system(.body, design: .rounded))
+                        .bold()
+                        .foregroundColor(Color(red: 251/255, green: 128/255, blue: 128/255))
+                }
+            }.padding(.top, 50)
+            Spacer()
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+struct FormField: View {
+    @Binding var fieldValue: String
+    
+    var fieldName: String = ""
+    var isSecure: Bool = false
+    
+    var body: some View {
+        VStack {
+            if isSecure {
+                SecureField(fieldName, text: $fieldValue)
+                    .font(.system(size: 20, weight: .semibold, design: .rounded))
+                    .padding(.horizontal)
+            } else {
+                TextField(fieldName, text: $fieldValue)
+                    .font(.system(size: 20, weight: .semibold, design: .rounded))
+                    .padding(.horizontal)
+            }
+            Divider()
+            .frame(height: 1)
+            .background(Color(red: 240/255, green: 240/255, blue: 240/255)) .padding(.horizontal)
+        }
+    }
+}
+
+struct RequirementText:View {
+    
+    var iconName = "xmark.square"
+    var iconColor = Color(red: 251/255, green: 128/255, blue: 128/255)
+    
+    var text = ""
+    var isStrikeThrough = false
+    
+    var body: some View {
+        HStack {
+            Image(systemName: iconName)
+                .foregroundColor(iconColor)
+            Text(text)
+                .font(.system(.body, design: .rounded))
+                .foregroundColor(.secondary)
+                .strikethrough(isStrikeThrough)
+            Spacer()
+        }
+    }
+}
---
+++ SwiftUIRegisterForm/SwiftUIRegisterForm/SwiftUIRegisterFormApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIRegisterFormApp.swift
+//  SwiftUIRegisterForm
+//
+//  Created by Marble Interactive SL on 17/1/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIRegisterFormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
